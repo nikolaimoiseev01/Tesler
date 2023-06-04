78b4f09a8b1c6eb7ad35856c51d1bbcbfb4ea0bc
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-06-04 15:04:45 +0300

    minor

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('UpdateGoods')->timezone('Europe/Moscow')->dailyAt('14:55');
+        $schedule->command('app:update-goods')->timezone('Europe/Moscow')->dailyAt('14:55');
     }
 
     /**
