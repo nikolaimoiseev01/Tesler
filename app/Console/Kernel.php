e3d5144fde261ba6e26ca6296fbf9f61e1dabdfc
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-11-07 14:07:18 +0300

    minor

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,18 +22,9 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-<<<<<<< HEAD
-        $schedule->command('app:update-goods')->timezone('Europe/Moscow')->dailyAt('15:07');
-=======
-        $schedule->command('UpdateGoods')->timezone('Europe/Moscow')->dailyAt('13:30');
-<<<<<<< HEAD
-        $schedule->command('UpdateService')->timezone('Europe/Moscow')->dailyAt('13:30');
-        $schedule->command('UpdateStaff')->timezone('Europe/Moscow')->dailyAt('13:30');
-=======
-        $schedule->command('ServiceUpdate')->timezone('Europe/Moscow')->dailyAt('13:30');
-        $schedule->command('StuffUpdate')->timezone('Europe/Moscow')->dailyAt('13:30');
->>>>>>> e3f482b26747b7b3b18e3b2bace1afd202ad8e79
->>>>>>> e0f7e9c111a7dc9488c1a2a834cfba24860dfdb2
+        $schedule->command('UpdateGoods')->timezone('Europe/Moscow')->dailyAt('21:00');
+        $schedule->command('UpdateStaff')->timezone('Europe/Moscow')->dailyAt('21:00');
+        $schedule->command('UpdateService')->timezone('Europe/Moscow')->dailyAt('21:00');
     }
 
     /**
