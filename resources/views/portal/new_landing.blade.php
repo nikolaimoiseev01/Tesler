9e3a73be5d011936aff10463d2b31baa456f44fd
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-02-09 19:20:09 +0300

    minor

--- resources/views/portal/new_landing.blade.php
+++ resources/views/portal/new_landing.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="welcome_wrap">
-        <img class="background_image" src="/media/media_fixed/abonement_cover2.png" alt="">
+        <img class="background_image" src="/media/media_fixed/new_landing_welcome.png" alt="">
 
         <div class="content">
             <div class="header_text_wrap">
@@ -31,49 +31,40 @@
 
     <div class="desc_wrap content">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum augue vitae mollis pretium. Donec
-            eleifend libero in enim ultricies porta. Donec congue, lorem rutrum ultrices ullamcorper, urna nibh aliquam
-            neque, sit amet posuere lectus sapien ut leo. Cras vestibulum mauris ut dignissim luctus. Suspendisse
-            laoreet neque enim. Fusce vel enim venenatis, pharetra arcu sit amet, rutrum purus. Phasellus a libero
-            tortor. Nulla volutpat euismod sagittis. Curabitur in dui eget tortor congue feugiat euismod nec justo.
-            Suspendisse sodales turpis ac lectus interdum, ac commodo urna dictum. In lobortis congue neque, vel feugiat
-            odio consectetur sed. Vestibulum ac pharetra arcu, ut tristique augue. Nunc vitae ligula non libero dictum
-            venenatis. Proin vehicula enim sed risus venenatis posuere. Mauris imperdiet dictum pretium. Sed leo mauris,
-            dignissim a justo ut, blandit malesuada metus.
+            Лучший подарок своему мужчине – это его женщина. Без будоражащей упаковки из кружева само собой не обойтись
+            – фантазия явно должна разыграться. Потому самые нескромные комплекты обязаны сыграть роль афродизиака в
+            желанный десерт. А как же насчет начинки? Предлагаем лишить его дара речи…Как? Как гласит истина из к/ф
+            «Грязные танцы» – «Сначала танцуй…» Приватный танец, манящие линии вокруг талии и желанная вы – его сон
+            наяву. Для полного боевого комплекта не забываем про чувственные локоны, изящные стрелы и влажный
+            блеск…хайлайтера. Включи себя!
         </p>
     </div>
 
     <div class="cards_wrap content">
         <div class="card_wrap">
-            <img src="/media/media_fixed/welcome_pic.jpg" alt="">
+            <img src="/media/media_fixed/new_landing_card_1.JPG" alt="">
             <h3>TESLER</h3>
             <p class="desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum augue vitae mollis pretium. Donec
-                eleifend libero in enim ultricies porta. Donec congue, lorem rutrum ultrices ullamcorper, urna nibh
-                aliquam
-                neque, sit amet posuere lectus sapien ut leo.
+                «Аккуратно превосходим других» – дерзко и вкусно о масштабном бьюти-проекте предпринимателя Юлии Теслер, зародившемся в 2019. TESLER BEAUTY – один из самых сильных игроков на сибирском рынке. Салоны красоты lux-сегмента.  Полный спектр услуг, премиальное качество и высококлассный сервис.
+
             </p>
         </div>
 
         <div class="card_wrap">
-            <img src="/media/media_fixed/welcome_pic.jpg" alt="">
+            <img src="/media/media_fixed/new_landing_card_2.JPG" alt="">
             <h3>INTENSO</h3>
             <p class="desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum augue vitae mollis pretium. Donec
-                eleifend libero in enim ultricies porta. Donec congue, lorem rutrum ultrices ullamcorper, urna nibh
-                aliquam
-                neque, sit amet posuere lectus sapien ut leo.
+                Лучшее украшение на твоем теле. Красноярский бренд нижнего белья ручной работы от легендарного сибирского креатора Натальи Калининой. Все коллекции лимитированы, чтобы каждая обладательница красивого комплекта белья чувствовала себя особенной и неповторимой.
+
             </p>
         </div>
 
         <div class="card_wrap">
-            <img src="/media/media_fixed/welcome_pic.jpg" alt="">
+            <img src="/media/media_fixed/new_landing_card_3.JPG" alt="">
             <h3>DAMER</h3>
             <p class="desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum augue vitae mollis pretium. Donec
-                eleifend libero in enim ultricies porta. Donec congue, lorem rutrum ultrices ullamcorper, urna nibh
-                aliquam
-                neque, sit amet posuere lectus sapien ut leo.
+                Педагог и хореограф самых женственных направлений strip , high heels. Прокачала более 500 учениц. Создатель танцевальных проектов для начинающих и профессиональных танцоров. Участница проекта «Танцы на ТНТ»
+
             </p>
         </div>
     </div>
@@ -92,10 +83,8 @@
             </div>
 
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum augue vitae mollis pretium. Donec
-                eleifend libero in enim ultricies porta. Donec congue, lorem rutrum ultrices ullamcorper, urna nibh
-                aliquam
-                neque, sit amet posuere lectus sapien ut leo.
+                Посетите салон TESLER BEAUTY с 10 по 16 февраля, записавшись на любую процедуру. Чем большее количество процедур Вы выбираете, тем больше именных жетонов вам присваивается. Дополнительные услуги также учитываются. Количество визитов в течение указанного периода не ограничено. 16 февраля мы раскручиваем виртуальный барабан с жетонами всех участников и рандомно выбираем победителя!
+
             </p>
         </div>
     </div>
