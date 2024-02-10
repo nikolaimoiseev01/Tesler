a7157c6977e8aedbde217fa256902230d9c4ee69
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-02-10 11:52:01 +0300

    minor

--- resources/views/layouts/new_landing.blade.php
+++ resources/views/layouts/new_landing.blade.php
@@ -17,6 +17,10 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400&family=Lora:ital@0;1&family=Tenor+Sans&display=swap" rel="stylesheet">
+
     <!-- Scripts -->
     @vite([
     'resources/sass/new_landing.scss',
