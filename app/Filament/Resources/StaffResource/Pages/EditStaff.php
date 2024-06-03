c727480a519d541e745567ae4697aafea5b8e458
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-06-03 13:30:48 +0300

    Update EditStaff.php

--- app/Filament/Resources/StaffResource/Pages/EditStaff.php
+++ app/Filament/Resources/StaffResource/Pages/EditStaff.php
@@ -14,7 +14,7 @@ class EditStaff extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+//            Actions\DeleteAction::make(),
         ];
     }
 
