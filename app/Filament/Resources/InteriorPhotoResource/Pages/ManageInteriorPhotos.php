15b794d2fb32741ea6e6c80798e9909fc8c73ce1
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-06-14 11:35:39 +0300

    Update ManageInteriorPhotos.php

--- app/Filament/Resources/InteriorPhotoResource/Pages/ManageInteriorPhotos.php
+++ app/Filament/Resources/InteriorPhotoResource/Pages/ManageInteriorPhotos.php
@@ -15,7 +15,7 @@ class ManageInteriorPhotos extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+//            Actions\CreateAction::make(),
         ];
     }
 }
