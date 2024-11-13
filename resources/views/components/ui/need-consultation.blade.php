04a8608363e236abd379e609f66ca2b131dac6fc
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-11-13 18:42:09 +0300

    Update need-consultation.blade.php

--- resources/views/components/ui/need-consultation.blade.php
+++ resources/views/components/ui/need-consultation.blade.php
@@ -10,6 +10,6 @@
         <a modal-id="consult_modal" class="modal-link link-bg fern">Бесплатная консультация</a>
     </div>
     <div class="image_blackout">
-        <img src="{{$this->imglink}}" alt="">
+        <img src="{{$imglink}}" alt="">
     </div>
 </div>
