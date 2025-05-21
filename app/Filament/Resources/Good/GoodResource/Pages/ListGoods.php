6ccf66e506b2cad3af2e3e2a0febb9b7cec6cdd4
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-05-21 15:02:50 +0300

    Minor

--- app/Filament/Resources/Good/GoodResource/Pages/ListGoods.php
+++ app/Filament/Resources/Good/GoodResource/Pages/ListGoods.php
@@ -3,9 +3,15 @@
 namespace App\Filament\Resources\Good\GoodResource\Pages;
 
 use App\Filament\Resources\Good\GoodResource;
+use App\Models\Good\Good;
+use App\Models\Service\Service;
+use App\Services\GoodYcOperations;
+use App\Services\ServiceYcOperations;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Widgets\StatsOverviewWidget;
+use Illuminate\Support\Facades\App;
 
 class ListGoods extends ListRecords
 {
@@ -14,7 +20,13 @@ class ListGoods extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Action::make("Refresh YC")
+                ->label('Синхронизироваться с YCLients')
+                ->requiresConfirmation()
+                ->modalDescription('Вы уверены, что хотите это сделать? Это займет несколько минут, так как проверит абсолютно все товары в системе.')
+                ->action(function (Service $service) {
+                    App::make(GoodYcOperations::class)->fullGoodsUpdate();
+                }),
         ];
     }
 
